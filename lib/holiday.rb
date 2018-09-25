82e9812da91f82bd2bcacdaa8f93e71cdb0dfce9
Samuel Cornet <samuel@noukod.com>
2018-09-25 15:03:46 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,7 +67,7 @@ def all_supplies_in_holidays(holiday_hash)
      puts " #{activity.to_s.split("_").collect {|i| i.capitalize}.join(" ")}: #{party.join(", ")}"
     end
   end
-  
+
 end
 
 def all_holidays_with_bbq(holiday_hash)
