4a0a048e85f619886022e6b2ca422660bb0c126e
Samuel Cornet <samuel@noukod.com>
2018-09-25 15:55:48 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,10 +73,10 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  # holiday_names =[]
+  holiday_names =[]
   holiday_hash.each do |season, holiday|
     # add_supply_to_memorial_day(holiday_hash, supply)
-    holiday_names =[]
+    
     holiday.each do |activity, party|
       if party.include?("BBQ")
        holiday_names << party
