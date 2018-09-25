4e3ffea592aa583dddf5446268e9bfe68ca353e7
Samuel Cornet <samuel@noukod.com>
2018-09-25 15:59:26 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,11 +75,9 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_names =[]
   holiday_hash.each do |season, holiday|
-    # add_supply_to_memorial_day(holiday_hash, supply)
-
     holiday.each do |activity, party|
       if party.include?("BBQ")
-       holiday_names << party
+        holiday_names << activity
       end
     end
   end
